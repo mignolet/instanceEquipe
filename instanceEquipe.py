58eb789efa5c8036e0961666b8dc7508b9fd8683
mignolet <mignoletgauthier@gmail.com>
2017-11-14 19:47:55 +0100

    google visio cloud
    google visio test

--- instanceEquipe.py
+++ instanceEquipe.py
@@ -1,3 +1,4 @@
+#-*- encoding:utf-8 -*-
 from json_responses import json_response, json_data, json_error
 from flask import Flask, request
 import json
@@ -7,32 +8,26 @@ from google.oauth2 import service_account
 import googleapiclient.discovery
 import base64
 
-
 app = Flask(__name__)
 app.debug = True
 
-APIKEY = os.environ.get("LOGINCOUCHDB")
-pwd = os.environ.get("PWDCOUCHDB")
-
-
-
+#check in visio cloud google
 def testGoogleVissio(imagefile):
     # Appel de la fonction de scan par Google
-    credentials = service_account.Credentials.from_service_account_file('VisionTest.json')
+    credentials = service_account.Credentials.from_service_account_file('/root/config/visio-cloud.json')
 
     # Authentification par Google
     service = googleapiclient.discovery.build('vision', 'v1', credentials=credentials)
 
     # Traitement de la photo
-
-    image_content = base64.b64encode(imagefile.read())
+    #image_content = base64.b64encode(imagefile.read())
 
     # Formattage de la requête en JSON pour Google Vision
 
     service_request = service.images().annotate(body={
         'requests': [{
             'image': {
-                'content': image_content.decode('UTF-8')
+                'content': imagefile.decode('UTF-8')
             },
             'features': [{
                 'type': 'LABEL_DETECTION',
@@ -50,32 +45,36 @@ def testGoogleVissio(imagefile):
 
     return label
 
-@app.route('/Classifier', methods="POST")
+#check picture objet
+#test in visio cloud google
+#insert couchdb objetfind
+@app.route('/Classifier')
 def Classifier():
-
+    #get picture
     imagefile = request.files['file']
     labelObjet = testGoogleVissio(imagefile)
-
+    #get data json
     jsonData = requests.get_json(force=True)
 
+    #check reponse
     if jsonData["nameImageFr"] == labelObjet:
-        idDevice = jsonData["Device_if"]
+        #check number objet find
         teamrequest = requests.get("https://couchdb.mignolet.fr/objetfinddb/_design/_all_obsearch/_view/all")
-
-    return "ok"
-
-
-#redirecte instance for team
-@app.route('/redirecte')
-def redirecte():
-    return 'redirecte'
+        print(teamrequest.json())
+        numObjetFind = json.loads(teamrequest.content)
+        print(numObjetFind["rows"][0]["value"])
+        #start json for couchdb
+        idDevice = jsonData["Device_id"]
+        jsonDataObjet = '{"idDevice": "'+str(idDevice)+'", "label": "'+labelObjet+'"}'
+        #insert in couchdb
+        r = requests.put("https://couchdb.mignolet.fr/objetfinddb/'objetFind34'", data=jsonDataObjet)
+        print(r.json())
+    return json_response(r.json(),r.status_code)
 
 
 def helloInscript():
-    dataName = requests.put("https://routerint.mignolet.fr/linkInstance")
-    print(dataName)
-
-
+    dataName = requests.get("https://routerint.mignolet.fr/linkInstance")
+    print(dataName.json())
 
 
 if __name__ == '__main__':
